9008d96e449072f9fee0a56648a1798e2d4123ef
dePauk <darjan.pavsic@gmail.com>
2017-07-16 18:31:33 +0200

    Add files via upload
    Odstranitev dela stare kode, za katerega sem ugotovil, da mi ne bo več prišel prav in sem ga imel prej pod "komentarjem" za vsak slučaj. Dodanih nekaj gumbov v oknu, ki se odpre prek tkInterja in ustvarjen princip določanja željene dolžine gesla na način "števca".

--- Generator_gesel.py
+++ Generator_gesel.py
@@ -1,55 +1,7 @@
-
 import random
 import tkinter as tk
 
 
-#class Geslo:
-#    
-#    def __init__(self, dolzina_gesla, vrsta_zapisa):
-#        self.dolzina_gesla = dolzina_gesla
-#        self.vrsta_zapisa = vrsta_zapisa
-#        
-#    def __repr__(self):
-#        return "Geslo dolžine {} v zapisu {}.".format(self.dolzina_gesla, self.vrsta_zapisa,)
-#        
-#    def zacni(self):
-#        self.dolzina_gesla = self.pridobi_dolzino_gesla()
-#        self.vrsta_zapisa = self.pridobi_vrsto_zapisa()
-#                
-#    def pridobi_dolzino_gesla(self):
-#        return int(input("Koliko znakov naj vsebuje geslo? "))
-#
-#    def pridobi_vrsto_zapisa(self):
-#        return int(input("Ali bi rad geslo iz cifer (0), iz malih črk (1), iz mešanih črk (2) ali mešanih znakov (3)?"))
-#     
-#
-#############################################################################################
-#        
-#    def generiraj_nakljucni_znak(self):
-#        if self.vrsta_zapisa == 0:
-#        elif self.vrsta_zapisa == 1:
-#            return random.choice("abcdefghijklmnopqrstuvwxwz")
-#        elif self.vrsta_zapisa == 2:
-#            return random.choice("abcdefghijklmnopqrstuvwxwzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-#        elif self.vrsta_zapisa == 3:
-#            return random.choice("0123456789abcdefghijklmnopqrstuvwxwzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-#
-#    def doloci_geslo(self, dolzina_gesla, vrsta_zapisa):
-#        sestavljanje_gesla = []
-#        for i in range(len(dolzina_gesla)):
-#            simbol = generiraj_nakljucni_znak(d)
-#            sestavljanje_gesla.append(simbol)
-#        return sestavljanje_gesla
-#
-#
-##############################################################################################
-#
-#
-#d = Geslo(None, None)
-#d.zacni()
-#d = Geslo(d.dolzina_gesla, d.vrsta_zapisa)
-#
-#print (d.doloci_geslo)
 
 
 
@@ -72,7 +24,7 @@ def nakljucno_geslo(dolzina,zapis):
 
 
 def program():
-
+    
     zelja = input("Kaj želiš (geslo)?" + "\n").lower()   
     if zelja == "geslo":
         
@@ -86,24 +38,7 @@ def program():
             print ("Napaka. Izbran mora biti način zapisa 0, 1, 2 ali 3." + "\n")
             program()
 
-#    elif zelja == "več gesel":
-#        koliko = int(input("Koliko gesel želiš?" + "\n"))
-#        if koliko < 2 or koliko > 10:
-#            print ("Napaka. Izbrano število generiranih gesel mora biti med 2 in 10." + "\n")
-#            program()
-#            
-#        dolz = int(input("Koliko znakov naj vsebuje geslo? " + "\n"))
-#        if dolz < 4 or dolz > 128:
-#            print ("Napaka. Geslo mora vsebovati med 4 in 128 znakov." + "\n")
-#            program()
-#            
-#        kodir = int(input("V katerem zapisu želiš geslo (0- cifre, 1- male črke, 2- mešane črke, 3- poljubno)? " + "\n"))
-#        if kodir != 0 and kodir != 1 and kodir != 2 and kodir != 3:
-#            print ("Napaka. Izbran mora biti način zapisa 0, 1, 2 ali 3." + "\n")
-#            program()
-        
-        
-            
+                        
         print ("\n" + nakljucno_geslo(dolz, kodir)+ "\n")
         if kodir == 0:
             print (round((10 ** dolz/(2*1000000000)/(3600*24)),1), "dni minimum Hack time per 1000 million/s" + "\n")
@@ -113,30 +48,72 @@ def program():
             print (round((52 ** dolz/(2*1000000000)/(3600*24)),1), "dni minimum Hack time per 1000 million/s" + "\n")
         elif kodir == 3:
             print (round((62 ** dolz/(2*1000000000)/(3600*24)),1), "dni minimum Hack time per 1000 million/s" + "\n")
-        #program()
-    else:
-        print ("Napaka." + "\n")
-        program()
+#        program()
+#    else:
+#        print ("Napaka." + "\n")
+#        program()
 
         
 def generiraj():
     program()
 
 
+
+trenutna_dolzina = 8
+
+def osvezi_prikaz():
+    prikaz_dolzine["text"] = str(trenutna_dolzina)
+    
+
+def povecaj_dolzino():
+    global trenutna_dolzina
+    trenutna_dolzina += 1
+    osvezi_prikaz()
+
+def zmanjsaj_dolzino():
+    global trenutna_dolzina
+    trenutna_dolzina -= 1
+    osvezi_prikaz()
+    
+    
+
+
 okno = tk.Tk()
 
-zgoraj = tk.Frame(okno)
-zgoraj.pack()
+naslov = tk.Frame(okno)
+naslov.pack()
+
+izbor_zapisa = tk.Frame(okno)
+izbor_zapisa.pack()
+
+stevilo_znakov = tk.Frame(okno)
+stevilo_znakov.pack()
+
+prikaz_dolzine = tk.Label(okno)
+prikaz_dolzine.pack()
+osvezi_prikaz()
+
 spodaj = tk.Frame(okno)
 spodaj.pack()
-zapis0 = tk.Button(zgoraj, text = "Samo cifre").grid(row=0, column=0)
-zapis1 = tk.Button(zgoraj, text = "Male črke").grid(row=0, column=1)
-zapis2 = tk.Button(zgoraj, text = "Mešane črke").grid(row=0, column=2)
-zapis3 = tk.Button(zgoraj, text = "Mešane črke + cifre").grid(row=0, column=3)
 
-gumb_generiraj = tk.Button(okno, text = "Generiraj geslo", command = generiraj).pack()
+izpis = tk.Frame()
+izpis.pack()
 
 
+zapis_naslova = tk.Label(naslov, text = "Nastavite željene vrednosti:").pack()
+
+zapis0 = tk.Button(izbor_zapisa, text = "Samo cifre").grid(row=0, column=0)
+zapis1 = tk.Button(izbor_zapisa, text = "Male črke").grid(row=0, column=1)
+zapis2 = tk.Button(izbor_zapisa, text = "Mešane črke").grid(row=0, column=2)
+zapis3 = tk.Button(izbor_zapisa, text = "Mešane črke + cifre").grid(row=0, column=3)
+
+zapis_za_dolzino = tk.Label(stevilo_znakov, text = "Število znakov:  ").grid(row=0, column=0)
+plus_gumb = tk.Button(stevilo_znakov, text = "+", command=povecaj_dolzino).grid(row=0, column=1)
+minus_gumb = tk.Button(stevilo_znakov, text = "-", command=zmanjsaj_dolzino).grid(row=0, column=2)
+
+gumb_generiraj = tk.Button(spodaj, text = "Generiraj geslo", command = generiraj).pack()
+
+izpis_gesla = tk.Label(izpis, text = "Še ne deluje. LP").pack()
 
 
 
