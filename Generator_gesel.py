c203663d40fd9a5da65577cdb951b691847d046a
dePauk <darjan.pavsic@gmail.com>
2017-07-18 19:47:15 +0200

    Add files via upload
    Dodan gumb, ki prikaže varnost gesla. Sicer je vse skupaj še rahlo nerodno, saj je treba po vsakem generiranju gesla pritisniti še na dodaten gumb, ki prikaže varnost le-tega.

--- Generator_gesel.py
+++ Generator_gesel.py
@@ -55,9 +55,82 @@ import tkinter as tk
 
 
         
-def generiraj():
-    program_tk()
+#def generiraj():
+#    program_tk()
 
+
+
+
+########################################################
+
+def nakljucno_geslo_tk(trenutna_dolzina,zapis_tk):
+    geslo = []
+    for i in range(trenutna_dolzina):
+    
+        if zapis_tk == 0:
+                i = random.choice("0123456789")
+        elif zapis_tk == 1:
+                i = random.choice("abcdefghijklmnopqrstuvwxwz")
+        elif zapis_tk == 2:
+                i = random.choice("abcdefghijklmnopqrstuvwxwzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        elif zapis_tk == 3:
+                i = random.choice("0123456789abcdefghijklmnopqrstuvwxwzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+           
+        geslo.append(i)
+    return "".join(geslo)
+
+
+
+
+
+def program_tk():
+    
+
+    dolz_tk = trenutna_dolzina
+    kodir_tk = zapis_tk 
+                        
+        #print ("\n" + nakljucno_geslo(dolz_tk, kodir_tk)+ "\n")
+    global generirano_geslo
+    generirano_geslo = nakljucno_geslo_tk(dolz_tk, kodir_tk)
+    
+    global hack_time
+    #print (generirano_geslo)
+    
+    
+    if kodir_tk == 0:
+        hack_time = round((10 ** dolz_tk/(2*1000000000)/(3600*24)),1)
+        pass
+            
+    elif kodir_tk == 1:
+        hack_time = round((26 ** dolz_tk/(2*1000000000)/(3600*24)),1)
+        pass
+            
+    elif kodir_tk == 2:
+        hack_time = round((52 ** dolz_tk/(2*1000000000)/(3600*24)),1)
+        pass
+            
+    elif kodir_tk == 3:
+        hack_time = round((62 ** dolz_tk/(2*1000000000)/(3600*24)),1)
+        pass
+    
+    if hack_time < 1000:
+        varnost_gesla_cca = "šibka"
+    else:
+        if hack_time < 10000:
+            varnost_gesla_cca = "zadostna"
+        else:
+            if hack_time < 100000:
+                varnost_gesla_cca = "dobra"
+            else:
+                if hack_time < 1000000:
+                    varnost_gesla_cca = "zelo dobra"
+                else:
+                    varnost_gesla_cca = "odlična"
+                
+
+
+
+####################################################
 
 
 trenutna_dolzina = 8
@@ -109,6 +182,7 @@ def osvezi_prikaz_zapisa():
     kateri_zapis["text"] = str(zapis_tk)
 
 
+
 generirano_geslo = "Za geslo pritisni na zgornji gumb."
 
 def osvezi_geslo():
@@ -118,67 +192,74 @@ def novo_geslo_tk():
     program_tk()
     global generirano_geslo
     osvezi_geslo()
-    
-    
-    
-
-
-########################################################
-
-def nakljucno_geslo_tk(trenutna_dolzina,zapis_tk):
-    geslo = []
-    for i in range(trenutna_dolzina):
-    
-        if zapis_tk == 0:
-                i = random.choice("0123456789")
-        elif zapis_tk == 1:
-                i = random.choice("abcdefghijklmnopqrstuvwxwz")
-        elif zapis_tk == 2:
-                i = random.choice("abcdefghijklmnopqrstuvwxwzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-        elif zapis_tk == 3:
-                i = random.choice("0123456789abcdefghijklmnopqrstuvwxwzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-           
-        geslo.append(i)
-    return "".join(geslo)
 
 
 
+varno_geslo = "?"
 
+def osvezi_varnost():
+    prikaz_varnosti_gesla["text"] = str(varno_geslo)
 
-def program_tk():
+def nova_varnost():
+    global varno_geslo
+    varno_geslo = str(kalkulator_varnosti())
+    osvezi_varnost()
     
 
-    dolz_tk = trenutna_dolzina
-    kodir_tk = zapis_tk 
-                        
-        #print ("\n" + nakljucno_geslo(dolz_tk, kodir_tk)+ "\n")
-    generirano_geslo = nakljucno_geslo_tk(dolz_tk, kodir_tk)
-    global generirano_geslo
-    #print (generirano_geslo)
+def kalkulator_varnosti():
+    global trenutna_dolzina
+    global hack_time
+    global zapis_tk
     
-    
-    if kodir_tk == 0:
-        #print (round((10 ** dolz/(2*1000000000)/(3600*24)),1), "dni minimum Hack time per 1000 million/s" + "\n")
+    if zapis_tk == 0:
+        hack_time = round((10 ** trenutna_dolzina/(2*1000000000)/(3600*24)),1)
         pass
             
-    elif kodir_tk == 1:
-        #print (round((26 ** dolz/(2*1000000000)/(3600*24)),1), "dni minimum Hack time per 1000 million/s" + "\n")
+    elif zapis_tk == 1:
+        hack_time = round((26 ** trenutna_dolzina/(2*1000000000)/(3600*24)),1)
         pass
             
-    elif kodir_tk == 2:
-        #print (round((52 ** dolz/(2*1000000000)/(3600*24)),1), "dni minimum Hack time per 1000 million/s" + "\n")
+    elif zapis_tk == 2:
+        hack_time = round((52 ** trenutna_dolzina/(2*1000000000)/(3600*24)),1)
         pass
             
-    elif kodir_tk == 3:
-        #print (round((62 ** dolz/(2*1000000000)/(3600*24)),1), "dni minimum Hack time per 1000 million/s" + "\n")
+    elif zapis_tk == 3:
+        hack_time = round((62 ** trenutna_dolzina/(2*1000000000)/(3600*24)),1)
         pass
-            
+    
+    if hack_time < 1000:
+        return "šibka"
+    else:
+        if hack_time < 10000:
+            return "zadostna"
+        else:
+            if hack_time < 100000:
+                return "dobra"
+            else:
+                if hack_time < 1000000:
+                    return "zelo dobra"
+                else:
+                    return "odlična"
 
 
 
-####################################################
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
 
 
 okno = tk.Tk()
@@ -207,6 +288,11 @@ izpis = tk.Label(okno)
 izpis.pack()
 osvezi_geslo()
 
+prikaz_varnosti_gesla = tk.Label(okno)
+prikaz_varnosti_gesla.pack()
+osvezi_varnost()
+
+
 
 zapis_naslova = tk.Label(naslov, text = "Nastavite željene vrednosti:").pack()
 
@@ -221,11 +307,11 @@ minus_gumb = tk.Button(stevilo_znakov, text = "-", command=zmanjsaj_dolzino).gri
 
 gumb_generiraj = tk.Button(spodaj, text = "Generiraj geslo", command = novo_geslo_tk).pack()
 
-#izpis_gesla = tk.Label(izpis, text= "").pack()
+gumb_varnost = tk.Button(spodaj, text = "Varnost", command = nova_varnost).pack()
 
 
 
-#program()
+
 
 
 
